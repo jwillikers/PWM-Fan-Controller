1f546f3fccc103a46c42597ffae88addcef94546
Jordan Williams <jordan@jwillikers.com>
2022-10-29 14:18:46 -0500

    Switch to the ATtiny85 MCU

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,27 @@
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
-use embedded_hal::digital::v2::OutputPin;
-use embedded_hal::PwmPin;
-use embedded_time::rate::*;
 use panic_halt as _;
 
-use adafruit_qt_py_rp2040::{
-    hal::{
-        // clocks::{
-        //     ClockManager,
-        // },
-        pac,
-        rosc,
-        Sio,
-    },
-    Pins,
-};
+use attiny_hal::simple_pwm::*;
+use attiny_hal::entry;
+use avr_hal_generic::simple_pwm::IntoPwmPin;
+use avr_hal_generic::simple_pwm::Prescaler;
+use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::PwmPin;
 
 #[entry]
 fn main() -> ! {
-    let mut pac = pac::Peripherals::take().unwrap();
+    let dp = attiny_hal::Peripherals::take().unwrap();
+    let pins = attiny_hal::pins!(dp);
+    let timer0 = Timer0Pwm::new(dp.TC0, Prescaler::Prescale64);
 
-    let clock = rosc::RingOscillator::new(pac.ROSC).initialize();
-    // let clocks = ClockManager::new(pac.CLOCKS);
-    // clocks.system_clock.configure_clock().map_err(InitError::ClockError)?;
+    // pb0 is pin #5
+    let mut pwm_fan = pins.pb0.into_output().into_pwm(&timer0);
+    pwm_fan.enable();
 
-    let sio = Sio::new(pac.SIO);
-
-    let pins = Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
-
-    pins.neopixel_power.into_push_pull_output().set_low().unwrap();
-
-    let mut pwm_slices = adafruit_qt_py_rp2040::hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
-    let pwm = &mut pwm_slices.pwm6;
-    pwm.set_ph_correct();
-    // Set the divider such that the frequency will operate at 25 khz.
-    pwm.set_div_int((clock.operating_frequency() / 25.kHz().integer()).integer() as u8);
-    pwm.enable();
-
-    let channel = &mut pwm.channel_b;
-    channel.output_to(pins.a0);
-    channel.set_duty(channel.get_max_duty() * 2 / 5);
+    // 40% of 255
+    pwm_fan.set_duty(102);
 
     loop {}
 }
