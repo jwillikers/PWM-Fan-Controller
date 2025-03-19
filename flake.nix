74e5c49a2c509bf82cb7632ad8331f67b6a557ff
Jordan Williams <jordan@jwillikers.com>
2025-03-19 11:46:45 -0500

    Try removing CARGO_TARGET_THUMBV6M_NONE_EABI_RUSTFLAGS

--- flake.nix
+++ flake.nix
@@ -206,7 +206,6 @@
               # See https://github.com/ipetkov/crane/issues/444
               # Should I symlink instead of copy?
               # ln --symbolic ${./memory.x} memory.x
-              CARGO_TARGET_THUMBV6M_NONE_EABI_RUSTFLAGS = "-C link-arg=--library-path=.";
               extraDummyScript = ''
                 cp --archive ${./boards/pico/memory.x} $out/memory.x
                 rm --force --recursive $out/src/bin/crane-dummy-*
